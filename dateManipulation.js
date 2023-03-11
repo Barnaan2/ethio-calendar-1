50c6091f853813370aa7968c6cdb6ff718142215
abel12-tech <myfirstgit9@gmail.com>
2023-03-11 16:30:51 +0300

    feat:feature is added

--- dateManipulation.js
+++ dateManipulation.js
@@ -13,5 +13,19 @@ class ethioCalendar{
 const obj = new ethioCalendar(12,3,2020).valueOf()
 console.log(typeof obj)
 
+ const dayInMonth = [30 , 31 ,30 ,31,31 ,28 ,31 ,30 ,31 ,30,31 ,31]
+ let today = new Date()
+ let day = today.getDate()
+ let month = today.getMonth()
 
+function dayDiff(day , month){
+   
+}
+
+function monthToDay(month){
+ let daySum = 0;
+for(let i = 0;i<=month;i++){
+   daySum += day
+}
+}
 
